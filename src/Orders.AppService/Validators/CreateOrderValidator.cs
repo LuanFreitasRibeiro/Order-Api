ad75dae1399fd95a0901ba7898c833935e271b03
Luan Freitas <luannr16@gmail.com>
2022-02-28 07:49:24 -0300

    implement unit tests

---
+++ src/Orders.AppService/Validators/CreateOrderValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using Orders.Domain.Models.Request.Order;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orders.AppService.Validators
+{
+    public class CreateOrderValidator : AbstractValidator<OrderRequest>
+    {
+        public CreateOrderValidator()
+        {
+            ValidateName();
+        }
+
+        private void ValidateName()
+        {
+            this.RuleFor(p => p.Price)
+                .NotEmpty()
+                .WithMessage("{PropertyName} is required.");
+        }
+    }
+}
