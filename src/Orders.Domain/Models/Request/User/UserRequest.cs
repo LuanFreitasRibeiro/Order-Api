416e70048cf443ca5c7b672e351e23aaaed27b43
Luan Freitas <luannr16@gmail.com>
2022-03-02 01:13:11 -0300

    adding user models

---
+++ src/Orders.Domain/Models/Request/User/UserRequest.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace Orders.Domain.Models.Request.User
+{
+    public class UserRequest
+    {
+        [Required]
+        [JsonProperty("username")]
+        public string Username { get; set; }
+
+        [Required]
+        [JsonProperty("password")]
+        public string Password { get; set; }
+    }
+}
